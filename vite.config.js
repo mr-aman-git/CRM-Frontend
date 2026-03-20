0139a7d92b9c05bcb66a33ddb99f5ecadc613a94
mr-aman-git <amanazr2003@gmail.com>
2026-03-20 18:20:28 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,38 @@
-import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "prompt",
-      injectRegister: false,
-      /* --- IS LINE KO ADD KAREIN --- */
-      selfDestroying: true,
-      
-      pwaAssets: {
-        disabled: true,
-        // disabled: false,
-        config: true,
-      },
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
 
-      manifest: {
-        name: "Ornsoar",
-        short_name: "Ornsoar",
-        description: "Ornsoar CRM",
-        theme_color: "#ffffff",
-      },
+    pwaAssets: {
+      disabled: false,
+      // disabled: false,
+      config: true,
+    },
 
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-      },
+    manifest: {
+      name: 'Ornsoar',
+      short_name: 'Ornsoar',
+      description: 'Ornsoar CRM',
+      theme_color: '#ffffff',
+    },
 
-      devOptions: {
-        enabled: false,
-        navigateFallback: "index.html",
-        suppressWarnings: true,
-        type: "module",
-      },
-    }),
-  ],
-});
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
